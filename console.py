45d5fe4b59dc4fb92016a2f39aed53154cddd22d
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-10 21:38:12 -0700

    Z11

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
@@ -13,6 +14,6 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-

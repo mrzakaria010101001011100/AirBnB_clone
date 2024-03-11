d40fdfb86fa1ccf9557a1268e3e2cbef038bf4b6
Badrnej <nejaa.badr@gmail.com>
2024-03-10 19:23:01 -0700

    B2

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,13 @@ class BaseModel:
     Defines the BaseModel class
     """
 
+    id_generator = uuid.uuid4
+
     def __init__(self):
         """
         Initializes a new instance of BaseModel
         """
-        self.id = str(uuid.uuid4())
+        self.id = str(BaseModel.id_generator())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -38,8 +40,8 @@ class BaseModel:
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        obj_dict['updated_at'] = self.updated_at.strftime('%Y-%m-%d %H:%M:%S')
         return obj_dict
 
 # Testing

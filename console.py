d133e81de03b810983641e7c96bfbfb7333b60b1
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-10 21:54:04 -0700

    Z13

--- console.py
+++ console.py
@@ -1,18 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""This module defines the HBNBCommand class, a command interpreter."""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
+    def help_quit(self):
+        """Print help message for the quit command."""
+        print("Quit command to exit the program")
+
     def do_EOF(self, arg):
-        """Exit the program gracefully"""
+        """Exit on EOF (Ctrl-D)."""
         print()
         return True
 
+    def emptyline(self):
+        """Do nothing on empty line."""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

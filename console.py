346b9dedbb0ed2dbd82c69f29892145c6c91e3fe
Badrnej <nejaa.badr@gmail.com>
2024-03-11 13:08:06 -0700

    B13

--- console.py
+++ console.py
@@ -1,19 +1,70 @@
 #!/usr/bin/python3
+"""
+Console 0.0.1
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Write a program called console.py that contains the entry point of the command interpreter:
+
+You must use the module cmd
+Your class definition must be: class HBNBCommand(cmd.Cmd):
+Your command interpreter should implement:
+quit and EOF to exit the program
+help (this action is provided by default by cmd but you should 
+keep it updated and documented as you work through tasks)
+a custom prompt: (hbnb)
+an empty line + ENTER shouldn’t execute anything
+Your code should not be executed when imported
+Warning:
+
+You should end your file with:
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+to make your program executable except when imported. Please don’t 
+add anything around - the Checker won’t like it otherwise
+
+guillaume@ubuntu:~/AirBnB$ ./console.py
+(hbnb) help
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+
+(hbnb) 
+(hbnb) help quit
+Quit command to exit the program
+
+(hbnb) 
+(hbnb) 
+(hbnb) quit 
+guillaume@ubuntu:~/AirBnB$ 
+No unittests needed
+
+Repo:
+
+GitHub repository: AirBnB_clone
+File: console.py
+"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """Exit the program gracefully"""
-        print()
+        """Exit the program"""
+        print("")
         return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+

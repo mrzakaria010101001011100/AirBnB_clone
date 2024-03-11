807e9136f1b58bca2a61ace99f7d5815231609a4
Badrnej <nejaa.badr@gmail.com>
2024-03-10 21:02:55 -0700

    B6

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,6 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the instance"""
         dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = self.created_at.isoformat()
         dict_copy['updated_at'] = self.updated_at.isoformat()
         return dict_copy

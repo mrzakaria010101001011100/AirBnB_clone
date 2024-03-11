a25e2044d819da3fa5a434d57bcddff318fa1476
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-03-10 19:32:02 -0700

    Z7

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,22 @@
 #!/usr/bin/python3
-import uuid
 from datetime import datetime
-from models import storage
+import uuid
 
 class BaseModel:
-    """BaseModel class"""
-
     def __init__(self, *args, **kwargs):
-        # Initialize instance attributes from kwargs if provided
         if kwargs:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    setattr(self, k, v)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
-
-    def save(self):
-        """Save method to update updated_at and save instance to storage"""
-        self.updated_at = datetime.now()
-        storage.save()
+            self.created_at = self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Convert object to dictionary"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
-# Test the BaseModel class
-if __name__ == "__main__":
-    # Creating an instance of BaseModel
-    model = BaseModel()
-
-    # Generating a dictionary representation of the instance
-    model_dict = model.to_dict()
-
-    # Re-creating an instance from the dictionary representation
-    new_model = BaseModel(**model_dict)
-
-    # Verifying the attributes of the new instance
-    print(new_model.__dict__)
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
